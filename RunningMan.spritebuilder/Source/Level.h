00e3e9ba807dfb14ac57c87c1cad6b7f48d936e3
swen2014 <steven2hddx@gmail.com>
2015-02-21 16:58:40 -0500

    set up gamescene

---
+++ RunningMan.spritebuilder/Source/Level.h
@@ -0,0 +1,13 @@
+//
+//  Level.h
+//  RunningMan
+//
+//  Created by steven on 2/21/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Level : CCNode
+
+@end
---
+++ RunningMan.spritebuilder/Source/Level.m
@@ -0,0 +1,13 @@
+//
+//  Level.m
+//  RunningMan
+//
+//  Created by steven on 2/21/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Level.h"
+
+@implementation Level
+
+@end
