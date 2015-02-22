c9d33594358c59ebe920f4d81516b76931a55fe6
swen2014 <steven2hddx@gmail.com>
2015-02-22 16:50:15 -0500

    0222

---
+++ RunningMan.spritebuilder/Source/Gameplay.m
@@ -0,0 +1,13 @@
+//
+//  Gameplay.m
+//  RunningMan
+//
+//  Created by steven on 2/22/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay
+
+@end
--- RunningMan.spritebuilder/Source/MainScene.m
+++ RunningMan.spritebuilder/Source/MainScene.m
@@ -2,4 +2,10 @@
 
 @implementation MainScene
 
+- (void)startGame {
+    CCScene *firstLevel = [CCBReader loadAsScene:@"Gameplay"];
+    CCTransition *transition = [CCTransition transitionFadeWithDuration:0.8f];
+    [[CCDirector sharedDirector] presentScene:firstLevel withTransition:transition];
+}
+
 @end
