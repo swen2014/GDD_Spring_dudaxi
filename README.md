6feedbca2764b97d6d97faa24d70cbb427b5f71c
swen2014 <steven2hddx@gmail.com>
2015-02-22 18:24:10 -0500

    0222 redo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GDD_Spring
+Crashed on 0222
+Rebuild from stratch
--- RunningMan.spritebuilder/Source/MainScene.m
+++ RunningMan.spritebuilder/Source/MainScene.m
@@ -2,10 +2,4 @@
 
 @implementation MainScene
 
-- (void)startGame {
-    CCScene *firstLevel = [CCBReader loadAsScene:@"Gameplay"];
-    CCTransition *transition = [CCTransition transitionFadeWithDuration:0.8f];
-    [[CCDirector sharedDirector] presentScene:firstLevel withTransition:transition];
-}
-
 @end
