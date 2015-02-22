48e75acc84449bc20cce266291a3ce8cd4bb08b8
swen2014 <steven2hddx@gmail.com>
2015-02-22 18:54:26 -0500

    Main Menu done, Robot and man created

---
+++ RunningMan.spritebuilder/Source/Man.h
@@ -0,0 +1,13 @@
+//
+//  Man.h
+//  RunningMan
+//
+//  Created by steven on 2/22/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Man : CCSprite
+
+@end
---
+++ RunningMan.spritebuilder/Source/Man.m
@@ -0,0 +1,21 @@
+//
+//  Man.m
+//  RunningMan
+//
+//  Created by steven on 2/22/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Man.h"
+
+@implementation Man
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Man created");
+    }
+    
+    return self;
+}
+@end
