38db9f31b88da036cc0c45dd6ad4d08c4af4350c
swen2014 <steven2hddx@gmail.com>
2015-02-22 19:47:01 -0500

    PlayScene established

--- RunningMan.spritebuilder/Source/MainScene.m
+++ RunningMan.spritebuilder/Source/MainScene.m
@@ -2,4 +2,11 @@
 
 @implementation MainScene
 
+- (void)startGame {     // Strat Button
+//    CCLOG(@"play button pressed");
+    CCScene *firstLevel = [CCBReader loadAsScene:@"Gameplay"];
+    CCTransition *transition = [CCTransition transitionFadeWithDuration:0.8f];
+    [[CCDirector sharedDirector] presentScene:firstLevel withTransition:transition];
+}
+
 @end
